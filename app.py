d42b0e96da4a67e2fbcc41d7418914d0ab0ea397
Duegan24 <62625959+Duegan24@users.noreply.github.com>
2020-05-29 21:23:36 -0500

    Final Code Submission

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 # Connect to and setup the connections to the SQLite database
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///hawaii.sqlite")  #?check_same_thread=False
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-
 
 # Setup references to the two databases
 Measurement = Base.classes.measurement
 Station = Base.classes.station
-
-session = Session(engine)
 
 #Create new Flask Instance
 app = Flask(__name__)
@@ -38,22 +35,37 @@ def welcome():
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
+
+    session = Session(engine)
+    
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
     precipitation = session.query(Measurement.date, Measurement.prcp).\
         filter(Measurement.date >= prev_year).all()
     precip = {date: prcp for date, prcp in precipitation}
+    
+    session.close()
+    
     return jsonify(precip)
 
 
 @app.route("/api/v1.0/stations")
 def stations():
+
+    session = Session(engine)
+
     results = session.query(Station.station).all()
     stations = list(np.ravel(results))
+    
+    session.close()
+    
     return jsonify(stations)
 
 
 @app.route("/api/v1.0/tobs")
 def temp_monthly():
+    
+    session = Session(engine)
+    
     prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
     results = session.query(Measurement.tobs).\
@@ -61,17 +73,23 @@ def temp_monthly():
         filter(Measurement.date >= prev_year).all()
 
     temps = list(np.ravel(results))
+
+    session.close()
+
     return jsonify(temps)
 
 
 @app.route("/api/v1.0/temp/<start>")
 @app.route("/api/v1.0/temp/<start>/<end>")
 def stats(start=None, end=None):
+
+    session = Session(engine)
+
     sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]           
 
     if not end: 
         results = session.query(*sel).\
-            filter(Measurement.date <= end).all()
+            filter(Measurement.date >= start).all()
         temps = list(np.ravel(results))
         return jsonify(temps)
 
@@ -79,6 +97,9 @@ def stats(start=None, end=None):
         filter(Measurement.date >= start).\
 	    filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
+
+    session.close()
+
     return jsonify(temps)
 
 if __name__ == "__main__":
